88b040aaef8ff5c339486ef5c69cb7c522713744
Kian Seong <kianseong_quah@icloud.com>
2025-06-04 20:13:28 +0800

    remove useless code

--- src/test/java/tech/kianseong/coffeebook/CoffeeBookApplicationTests.java
+++ src/test/java/tech/kianseong/coffeebook/CoffeeBookApplicationTests.java
@@ -1,13 +1,8 @@
 package tech.kianseong.coffeebook;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoffeeBookApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
